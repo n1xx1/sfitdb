f87ccbbb02c6db7bae8cdb23a2e020925f5be22c
n1xx1 <me@simonemiraglia.it>
2025-10-20 11:50:01 +0200

    refactor scripts, improve sidebar styles

--- site/assets/js/main.ts
+++ site/assets/js/main.ts
@@ -51,17 +51,4 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-function setTheme(theme: string) {
-  document.documentElement.classList.remove("light", "dark");
-  if (theme !== "light" && theme !== "dark") {
-    theme = window.matchMedia("(prefers-color-scheme: dark)").matches
-      ? "dark"
-      : "light";
-  }
-  document.documentElement.classList.add(theme);
-  document.documentElement.style.colorScheme = theme;
-}
-
-setTheme(localStorage.getItem("color-theme") ?? "system");
-
-(globalThis as any)._setTheme = setTheme;
+export {};
