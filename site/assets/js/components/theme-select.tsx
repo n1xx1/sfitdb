1030bd3a9d105b264400f93af8b7b3393ffa2fa5
n1xx1 <me@simonemiraglia.it>
2025-11-14 18:45:50 +0100

    fix theme select hydration error

--- site/assets/js/components/theme-select.tsx
+++ site/assets/js/components/theme-select.tsx
@@ -1,35 +1,24 @@
-import { useCallbackRef } from "@/hooks/use-callback-ref";
 import { SelectTrigger } from "@radix-ui/react-select";
 import { ContrastIcon, MoonIcon, SunIcon } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useEffect, useSyncExternalStore } from "react";
 import { Button } from "./ui/button";
 import { Select, SelectContent, SelectItem, SelectValue } from "./ui/select";
 
 export default function ThemeSelect() {
-  const [theme, setTheme] = useState(() =>
-    typeof window !== "undefined"
-      ? (window.localStorage.getItem("color-theme") ?? "system")
-      : "system",
+  const theme = useSyncExternalStore(
+    themeStore.subscribe,
+    themeStore.getSnapshot,
+    () => "system",
   );
-
-  const updateTheme = useCallbackRef((theme: string) => {
-    (globalThis as any)._setTheme(theme);
-    setTheme(theme);
-    localStorage.setItem("color-theme", theme);
-  });
 
   useEffect(() => {
     window
       .matchMedia("(prefers-color-scheme: dark)")
-      .addEventListener("change", () => {
-        if (localStorage.getItem("color-theme") === "system") {
-          (globalThis as any)._setTheme(theme);
-        }
-      });
+      .addEventListener("change", () => themeStore.sync());
   }, []);
 
   return (
-    <Select value={theme} onValueChange={(value) => updateTheme(value)}>
+    <Select value={theme} onValueChange={(value) => themeStore.update(value)}>
       <SelectTrigger asChild>
         <Button variant="ghost" size="icon">
           <SelectValue>
@@ -57,3 +46,25 @@ export default function ThemeSelect() {
     </Select>
   );
 }
+
+const themeStore = {
+  getSnapshot() {
+    return localStorage.getItem("color-theme") ?? "system";
+  },
+  subscribe(cb: () => void) {
+    const listener = (event: StorageEvent) =>
+      event.key === "color-theme" && cb();
+    window.addEventListener("storage", listener);
+    return () => void window.removeEventListener("storage", listener);
+  },
+  update(theme: string) {
+    localStorage.setItem("color-theme", theme);
+    window.dispatchEvent(
+      new StorageEvent("storage", { key: "color-theme", newValue: theme }),
+    );
+    this.sync();
+  },
+  sync() {
+    (globalThis as any)._setTheme(this.getSnapshot());
+  },
+};
